c7e4d7e8f4bd5a613c1508f456ac3e0940e4f03c
Reid Vandewiele <marut@cat.pdx.edu>
2012-06-15 11:26:44 -0700

    Code cleanup; fix deprecation notices

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@
 #
 
 class resolver (
-  $domainname  = "$domain", 
+  $domainname  = $::domain,
   $searchpath  = undef,
   $nameservers = undef,
   $options     = undef,
@@ -15,49 +15,50 @@ class resolver (
   include resolver::params
 
   $specialoptions = $options or $sortlist
+  $ensure_special = $specialoptions ? {
+    true    => file,
+    default => absent,
+  }
 
   ##
   # Virtual files. declared here because some files use templating
   # and variables in this scope.
   @file {
-    "resolver/manual_resolv_conf":
+    'resolver/manual_resolv_conf':
+      ensure  => $ensure_special,
       path    => $resolver::params::dhclientoverride,
-      ensure  => $specialoptions ? {
-        true    => file,
-        default => absent,
-      },
-      owner   => "root",
-      group   => "root",
-      mode    => "755",
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0755',
       content => "#!/bin/sh\nmake_resolv_conf () { :; }\n";
-    "resolver/resolv.conf":
-      path    => "/etc/resolv.conf",
-      owner   => "root",
-      mode    => "644",
-      content => template("resolver/resolv.conf.erb");
-    "resolver/dhclient.conf":
+    'resolver/resolv.conf':
+      path    => '/etc/resolv.conf',
+      owner   => 'root',
+      mode    => '0644',
+      content => template('resolver/resolv.conf.erb');
+    'resolver/dhclient.conf':
       path    => $resolver::params::dhclientfile,
-      owner   => "root",
-      group   => "root",
-      mode    => "644",
-      content => template("resolver/dhclient.conf.erb");
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => template('resolver/dhclient.conf.erb');
   }
 
   ##
   # If this is ubuntu, don't rewrite the resolv.conf file when using dhcp.
   # If you want it rewritten, don't declare it manually in puppet.
-  case $operatingsystem {
-    "Ubuntu": { 
+  case $::operatingsystem {
+    'Ubuntu': {
       if $options == undef and $sortlist == undef {
-        realize File["resolver/dhclient.conf"]
+        realize File['resolver/dhclient.conf']
       } else {
-        realize File["resolver/dhclient.conf"]
-        realize File["resolver/manual_resolv_conf"]
-        realize File["resolver/resolv.conf"]
+        realize File['resolver/dhclient.conf']
+        realize File['resolver/manual_resolv_conf']
+        realize File['resolver/resolv.conf']
       }
     }
     default:  {
-      realize File["resolver/resolv.conf"]
+      realize File['resolver/resolv.conf']
     }
   }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,20 +1,20 @@
 class resolver::params {
-  case $operatingsystem {
-    "Ubuntu": {
-      case $lsbdistcodename {
-        "lucid", "jaunty": {
-          $dhclientfile     = "/etc/dhcp3/dhclient.conf"
-          $dhclientoverride = "/etc/dhcp3/dhclient-enter-hooks.d/manual_resolv_conf"
+  case $::operatingsystem {
+    'Ubuntu': {
+      case $::lsbdistcodename {
+        'lucid', 'jaunty': {
+          $dhclientfile     = '/etc/dhcp3/dhclient.conf'
+          $dhclientoverride = '/etc/dhcp3/dhclient-enter-hooks.d/manual_resolv_conf'
         }
         default: {
-          $dhclientfile     = "/etc/dhcp/dhclient.conf"
-          $dhclientoverride = "/etc/dhcp/dhclient-enter-hooks.d/manual_resolv_conf"
+          $dhclientfile     = '/etc/dhcp/dhclient.conf'
+          $dhclientoverride = '/etc/dhcp/dhclient-enter-hooks.d/manual_resolv_conf'
         }
       }
     }
     default: {
-      $dhclientfile     = "/etc/dhcp/dhclient.conf"
-      $dhclientoverride = "/etc/dhcp/dhclient-enter-hooks.d/manual_resolv_conf"
+      $dhclientfile     = '/etc/dhcp/dhclient.conf'
+      $dhclientoverride = '/etc/dhcp/dhclient-enter-hooks.d/manual_resolv_conf'
     }
   }
 }
